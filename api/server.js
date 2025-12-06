ea1eccbde3a4c808b882449f05f7124ed19e5acd
Afzaal <afzaalzaydan908@gmail.com>
2025-12-06 09:36:49 +0700

    fix bug

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware CORS
 app.use(cors({
   // Izinkan origin spesifik dari frontend Vite Anda
-  origin: ['http://localhost:5173','https://absensi-pekerja-fe.vercel.app','https://absensi-pekerja-fe-git-main-afzaals-projects-c2614662.vercel.app'],
+  origin: ['http://localhost:5173','https://absensi-pekerja-fe.vercel.app'],
   credentials: true // Penting jika Anda menggunakan cookie/session
 }));
 
@@ -43,3 +43,4 @@ app.use("/api/attendance", attendanceRoutes);
 // Jalankan server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  
